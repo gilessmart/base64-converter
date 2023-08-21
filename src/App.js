527ff3e3b33df970454bfeb4a8f4d9ccb66b492e
Giles Smart <gilessmart@hotmail.com>
2023-08-21 14:34:55 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Utf8 from 'crypto-js/enc-utf8';
+import Base64 from 'crypto-js/enc-base64';
 
-function App() {
+export default function App() {
+  const [text, setText] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  function onTextChange(event) {
+    const newText = event.target.value;
+    setText(newText);
+  }
+
+  function onEncodeClick() {
+    try {
+      const uft8Words = Utf8.parse(text);
+      const base64 = Base64.stringify(uft8Words);
+      setText(base64);
+    }
+    catch (error) {
+      setErrorMessage(error.message);
+    }
+  }
+
+  function onDecodeClick() {
+    try {
+      const uft8Words = Base64.parse(text);
+      const decodedText = Utf8.stringify(uft8Words);
+      setText(decodedText);
+    }
+    catch (error) {
+      setErrorMessage(error.message);
+    }
+  }
+
+  function onDismissErrorClick() {
+    setErrorMessage(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app'>
+      <header>
+        <h1 className='mainHeading'>Base64 Converter</h1>
+        
       </header>
+
+      <p className="subheading">Converts strings to / from base64 encoding</p>
+
+      <main>
+        {errorMessage && <div class="notification error" role='alert'>
+          <p className='message'>{errorMessage}</p>
+          <button className="dismiss" onClick={onDismissErrorClick}>Dismiss</button>
+        </div>}
+
+        <textarea className='text-editor' 
+                  placeholder='Text to convert'
+                  value={text}
+                  onChange={onTextChange} />
+
+        <div className="controls">
+          <button disabled={text.length === 0} onClick={onEncodeClick}>Encode</button>
+          <button disabled={text.length === 0} onClick={onDecodeClick}>Decode</button>
+        </div>
+      </main>
     </div>
   );
-}
-
-export default App;
+};
